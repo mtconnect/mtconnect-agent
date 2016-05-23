74cc255b71f4bd4126cc71c670b59d090fbd5633
Shakthi Kannan <shakthi@systeminsights.com>
2016-05-23 17:45:24 +0530

    Added .gitignore, package.json and {main, config, server, logger}.js

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+const env = process.env;
+
+module.exports = {
+    app: {
+        name:    "svc-agent-reader",
+        version: env.VI_VERSION || console.log("WARN: env.VI_VERSION not set " + "unknown"),
+    },
+    logging: {
+        logLevel: env.VI_LOG_LEVEL || "warn",
+        logDir:   env.VI_LOG_DIR,
+    }
+};
