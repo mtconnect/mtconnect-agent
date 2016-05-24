ea1093cc7b6d0257416081254b7a19dd56e065eb
Shakthi Kannan <shakthi@systeminsights.com>
2016-05-24 16:41:52 +0530

    Update logging and obtain local IP address

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,1 +1,9 @@
 const config = require("./config");
+const bunyan = require("bunyan");
+
+module.exports = bunyan.createLogger({
+    name:     config.app.name,
+    version:  config.app.version,
+    logDir:   config.logging.logDir,
+    loglevel: config.logging.logLevel
+});
--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 const log = require("./config/logger");
+const init = require("./init");
+
+var ipAddress = init.getIP();
+
+log.warn(ipAddress)
