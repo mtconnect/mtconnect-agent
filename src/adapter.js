381c72ce580069e168d84d272a2770701d814201
Shakthi Kannan <shakthi@systeminsights.com>
2016-05-26 17:52:38 +0530

    Serve device definition file and machine data

--- src/adapter.js
+++ src/adapter.js
@@ -1,6 +1,11 @@
-const log = require("./config/logger");
+const log  = require("./config/logger");
 const init = require("./init");
-const ssdp = require("node-ssdp").Server
+
+/*
+ *  SSDP
+ */
+
+const ssdp = require("node-ssdp").Server;
 
 var adapter = new ssdp();
 
@@ -20,3 +25,52 @@ adapter.start();
 process.on('exit', function () {
     adapter.stop();
 });
+
+/*
+ *  Serve Device definition file
+ */
+
+const SERVE_FILE_PORT = 8080;
+const node_static = require("node-static");
+
+var file = new node_static.Server("./public");
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        /*
+         *  Serve files!
+         */
+        file.serve(request, response);
+    }).resume();
+}).listen(SERVE_FILE_PORT);
+
+log.info("Starting HTTP web server on port %d", SERVE_FILE_PORT);
+
+/*
+ *  Machine data
+ */
+
+const net   = require("net");
+
+const MACHINE_PORT = 8081;
+
+function* machineDataGenerator() {
+    yield '2|execution|INTERRUPTED\r\n';
+    yield '2|tool_id|1\r\n';
+    yield '2|execution|ACTIVE\r\n';
+}
+
+var machine = net.createServer();
+
+var machineData = machineDataGenerator();
+
+machine.on('connection', (socket) => {
+
+    data = machineData.next().value;
+    if (data) { socket.write(data) };
+
+});
+
+machine.listen(MACHINE_PORT, "localhost");
+
+log.info("Starting machine TCP server on port %d", MACHINE_PORT);
