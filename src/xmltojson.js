18a6eeed4a5b5ce22b6d8e1c7ab58aff93222724
Rosmi <rosmi@systeminsights.com>
2016-05-27 17:39:22 +0530

    added test for device schema insertion and included required files in checkfile

--- src/xmltojson.js
+++ src/xmltojson.js
@@ -2,7 +2,7 @@ var fs = require ('fs');
 var xml2js = require('xml2js');
 var util = require ('util');
 const shdrcollection = require("./lokijs");
-
+var mtcdevices = shdrcollection.getschemaDB();
 //xml device schema to json conversion
 function xmltojson(xmlobj){
 
@@ -32,7 +32,7 @@ function insertschematoDB(parseddata){
     for (var i = 0; i < devicelength; i++){
       uuid[i] =  parseddata.MTConnectDevices.Devices[0].Device[i].$.uuid;
       device[i] = parseddata.MTConnectDevices.Devices[0].Device[i];
-      mtcdevices.insert({xmlns: xmlns, time: timeval, uuid: uuid[i], device: device[i]})
+      mtcdevices.insert({xmlns: xmlns, time: timeval, uuid: uuid[i], device: device[i]});
     }
   }
   return mtcdevices;
@@ -40,14 +40,13 @@ function insertschematoDB(parseddata){
 
 // read xml file
 var xml = fs.readFileSync('E:/specimpl/readermodule/Devices2di.xml','utf8');
-var mtcdevices = shdrcollection.getschemaDB();
 var jsonobj = xmltojson(xml);
 var jsonfile =  fs.writeFileSync('E:/Devices2di.json',JSON.stringify(jsonobj),'utf8');
 
 //console.log(util.inspect(jsonobj.MTConnectDevices.Devices[0].Device[0].DataItems[0].DataItem[1].$.type, false, null));
 //var jsonobj = fs.readFileSync('E:/Devices2di.json','utf8');
-//var insertedschema = insertschematoDB(jsonobj);
-//console.log(util.inspect(jsonobj, false, null));
+var insertedschema = insertschematoDB(jsonobj);
+//console.log(util.inspect(insertedschema, false, null));
 
 module.exports = {
   xmltojson,
