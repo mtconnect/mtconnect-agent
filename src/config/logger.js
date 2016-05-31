c45ed001136ce6b55b2b7b7156df5db395ea897d
Shakthi Kannan <shakthi@systeminsights.com>
2016-05-31 12:57:14 +0530

    Added no-console exception rule for eslint with fixed source files

--- src/config/logger.js
+++ src/config/logger.js
@@ -2,8 +2,8 @@ const config = require('./config');
 const bunyan = require('bunyan');
 
 module.exports = bunyan.createLogger({
-    name:     config.app.name,
-    version:  config.app.version,
-    logDir:   config.logging.logDir,
-    loglevel: config.logging.logLevel
+  name: config.app.name,
+  version: config.app.version,
+  logDir: config.logging.logDir,
+  loglevel: config.logging.logLevel,
 });
