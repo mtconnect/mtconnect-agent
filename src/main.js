c67f6f939e699fe3eafe6c6a1d83712c42360da2
Will Sobel <will@systeminsights.com>
2016-07-22 17:47:44 -0700

    Changed current to probe.

--- src/main.js
+++ src/main.js
@@ -157,7 +157,7 @@ setInterval(() => {
   });
 }, PING_INTERVAL);
 
-app.get('/current', (req, res) => {
+app.get('/probe', (req, res) => {
   const circularBufferPtr = dataStorage.circularBuffer;
   const latestSchema = lokijs.searchDeviceSchema(uuid);
   const dataItemsWithVal = dataStorage.getDataItem(latestSchema, circularBufferPtr);
