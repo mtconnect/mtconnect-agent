72dd8f4f213db5510ab3c312af7da985da3c5f22
Rosmi <rosmi@systeminsights.com>
2016-07-25 12:36:02 +0530

    fixed eslint error in adapterTest

--- test/adapterTest.js
+++ test/adapterTest.js
@@ -78,7 +78,8 @@ describe('dataExists', () => {
     it('must return data', () => {
       const machineData = adapter.machineDataGenerator();
 
-      assert.equal(adapter.dataExists(machineData), '2016-07-25T05:50:19.303002Z|avail|UNAVAILABLE');
+      assert.equal(adapter.dataExists(machineData),
+      '2016-07-25T05:50:19.303002Z|avail|UNAVAILABLE');
     });
   });
 
