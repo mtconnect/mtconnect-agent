a2fda3ace2bc12e75b5cd1934dde3f339b62b817
Rosmi <rosmi@systeminsights.com>
2016-09-08 16:22:58 +0530

    added test, edited test, edited readfromHashCurrent and hashLast

--- src/dataStorage.js
+++ src/dataStorage.js
@@ -217,9 +217,10 @@ function getSequence() {
   *
   */
 function readFromHashLast(idVal, path ) {
-  const result = hashLast.get(idVal);
+  let result = hashLast.get(idVal);
   if (path) {
-    if (result.path.includes(path)) {
+    result = filterPath([result],path);
+    if (!R.isEmpty(result)) {
       return result;
     }
     return;
@@ -241,10 +242,11 @@ function readFromHashLast(idVal, path ) {
   *
   */
 function readFromHashCurrent(idVal, path) {
-  const result = hashCurrent.get(idVal);
+  let result = hashCurrent.get(idVal);
   if (path) {
-    if (result.path.includes(path)) {
-      return result;
+    result = filterPath([result],path);
+    if (!R.isEmpty(result)) {
+      return result[0];
     }
     return;
   }
