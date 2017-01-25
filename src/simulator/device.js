ced53c87886c09066a221442bda6cda9d33c7f17
Andrew Shatnyy <AndrewShatnyy@gmail.com>
2017-01-24 21:51:59 -0800

    pipe device data as even-stream with time outs

--- src/simulator/device.js
+++ src/simulator/device.js
@@ -1,5 +1,5 @@
 const log = require('../config/logger');
-const { PassThrough } = require('stream');
+const through = require('through');
 // const common = require('../common');
 const config = require('../config/config');
 const byline = require('byline');
@@ -48,24 +48,34 @@ const { inputFile, machinePort } = config.app.simulator;
 //   common.processError(`${err}`, true);
 // });
 
-// logger
 
-app.use(function *logger(next) {
-  const start = new Date;
-  yield next;
-  const ms = new Date - start;
-  log.info('%s %s - %s', this.method, this.url, ms);
+// send sends line with a delay to the client
+// line [String] single line of the input file
+function send(line) {
+  this.queue(line);
+  this.pause();
+  const to = setTimeout(() => {
+    clearTimeout(to);
+    this.resume();
+  }, 1000);
+}
+
+// end finigshes the stream
+function end() { this.queue(null); }
+
+app.on('error', (err, ctx) => {
+  log.error('server error', err, ctx);
 });
-
-// response
-
-const stream = fs.createReadStream(inputFile);
 
 app.use(function *response() {
+  this.type = 'text/event-stream; charset=utf-8';
+  this.set('Cache-Control', 'no-cache');
+  this.set('Connection', 'keep-alive');
+  const stream = fs.createReadStream(inputFile);
   this.body = byline.createStream(stream)
     .on('error', this.onerror)
-    .pipe(PassThrough());
+    .pipe(through(send, end));
 });
 
-app.listen(machinePort);
-console.info(`starting server on localhost:${machinePort}`);
+
+app.listen(machinePort, '0.0.0.0', () => log.info(`Running device on ${machinePort}`));
