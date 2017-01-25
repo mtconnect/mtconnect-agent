d53c33221c08f22308c345a2dbc83d50723bd439
Andrew Shatnyy <AndrewShatnyy@gmail.com>
2017-01-24 21:57:19 -0800

    handle socket exits

--- src/simulator/device.js
+++ src/simulator/device.js
@@ -8,47 +8,6 @@ const app = koa();
 const fs = require('fs');
 const { inputFile, machinePort } = config.app.simulator;
 
-/**
-  * writeDataLoop() sends machine data to the Agent in loop
-  *
-  * @param {Object} socket
-  * @param {Object} count
-  * @param {Object} delay
-  */
-// function writeDataLoop(socket, countValue, delay) {
-//   let count = countValue;
-//   while (count) {
-//     lineReader.eachLine(inputFile, (line) => {
-//       setTimeout(() => {
-//         try {
-//           socket.write(`${line}\n`);
-//         } catch (e) {
-//           common.processError(`Error: ${e}`, false);
-//         }
-//       }, Math.floor(Math.random() * delay)); // Simulate delay
-//     });
-//     count = count - 1;
-//   }
-// }
-
-
-/**
- * Simulator (adapter)
- */
-
-// machine.on('connection', (socket) => {
-//   log.debug('Machine connected');
-//   writeDataLoop(socket, 100, 10000);
-//   socket.on('data', (data) => {
-//     console.log('Received:', data.toString());
-//   })
-// });
-
-// machine.on('error', (err) => {
-//   common.processError(`${err}`, true);
-// });
-
-
 // send sends line with a delay to the client
 // line [String] single line of the input file
 function send(line) {
@@ -72,9 +31,21 @@ app.use(function *response() {
   this.set('Cache-Control', 'no-cache');
   this.set('Connection', 'keep-alive');
   const stream = fs.createReadStream(inputFile);
-  this.body = byline.createStream(stream)
+  const lineFeed = byline.createStream(stream);
+  this.body = lineFeed
     .on('error', this.onerror)
     .pipe(through(send, end));
+
+  const socket = this.socket;
+
+  function close() {
+    lineFeed.unpipe();
+    socket.removeListener('error', close);
+    socket.removeListener('close', close);
+  }
+
+  socket.on('error', close);
+  socket.on('close', close);
 });
 
 
