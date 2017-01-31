515935f5f31bf410c9a6dbd6c80e6bbde953fbe9
Andrew Shatnyy <AndrewShatnyy@gmail.com>
2017-01-30 21:44:25 -0800

    bootsrap the agent move handlers into separate module for refactroing

--- src/agent.js
+++ src/agent.js
@@ -4,14 +4,13 @@
 // https://github.com/diversario/node-ssdp/issues/70
 // it is ok but unpredictable when testing
 
-const log = require('../config/logger');
-const config = require('../config/config');
+const log = require('./config/logger');
+const config = require('./config/config');
 const ip = require('ip').address();
-const { uuid, urn, machinePort, filePort } = config.app.simulator;
+// const { uuid, urn, machinePort, filePort } = config.app.simulator;
 const { urnSearch, agentPort, path, allowPut, AllowPutFrom } = config.app.agent;
-const koa = requrie('koa');
+const koa = require('koa');
 const app = koa();
-
 
 function defineAgentServer() { // TODO check for requestType 'get' and 'put'
   // handles all the incoming request
@@ -20,7 +19,7 @@ function defineAgentServer() { // TODO check for requestType 'get' and 'put'
   app.use(bodyParser.json());
 
   app.all('*', (req, res) => {
-    log.debug(`Request ${req.method} from ${req.get('host')}:`)
+    log.debug(`Request ${req.method} from ${req.get('host')}:`);
     let acceptType;
     if (req.headers.accept) {
       acceptType = req.headers.accept;
@@ -33,9 +32,24 @@ function defineAgentServer() { // TODO check for requestType 'get' and 'put'
   });
 }
 
+app.use(function *(next) {
+  const start = new Date;
+  yield next;
+  const ms = new Date - start;
+  console.info('%s %s - %s', this.method, this.url, ms);
+});
+
+app.use(function *() {
+  this.body = 'Hello World';
+});
 
 function start() {
-  return new Promise((success) => app.listen(agentPort, ip, success));
+  return new Promise((success) => {
+    app.listen(agentPort, '0.0.0.0', () => {
+      console.info(`Starting agent on port: ${agentPort}`);
+      success();
+    });
+  });
 }
 
 function stop() {
