81492f15d3281c953087bee83988d43c54a66e5c
Dima Kurbatov <dskurbatov13@gmail.com>
2017-07-01 19:06:16 -0700

    added new route to agent schema.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const probe = require('./probe')
 const sample = require('./sample')
 const assets = require('./assets')
 const current = require('./current')
+const schema = require('./schema')
 
 module.exports = (router) => {
   probe(router)
   assets(router)
   sample(router)
   current(router)
+  schema(router)
 }
