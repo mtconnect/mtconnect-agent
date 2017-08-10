8f1a6ae19baec994611610895348fcf68963b6d6
Dima Kurbatov <dskurbatov13@gmail.com>
2017-08-09 18:07:59 -0700

    added devices.on(delete)

--- src/store.js
+++ src/store.js
@@ -1,4 +1,12 @@
 // devices absraction
 const Loki = require('lokijs')
+const lokijs = require('./lokijs')
+
 const Db = new Loki('agent-loki.json')
-module.exports = Db.addCollection('devices')
+const devices = Db.addCollection('devices')
+
+devices.on('delete', (obj) => {
+  lokijs.updateBufferOnDisconnect(obj.uuid)
+})
+
+module.exports = devices
